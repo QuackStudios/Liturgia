a34571905334858caeb6237f03952dafd58793b2
NicoS3006 <dominic@quackstudios.com.au>
2025-01-28 10:05:20 +1000

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Ensures React errors are shown in development
-  
-    // Image optimization configuration
-    images: {
-      loader: 'default', // Default Next.js loader
-      path: '/assets/_next/image', // Path for optimized images
-    },
-  
-    // Base path or asset prefix in case your site is deployed in a subfolder
-    basePath: '',
-    assetPrefix: '',
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true, // Ensures React errors are shown in development
+
+  // Image optimization configuration
+  images: {
+    loader: 'default', // Default Next.js loader
+    path: '/Liturgia/assets/_next/image', // Updated path for optimized images
+  },
+
+  // Base path or asset prefix for GitHub Pages
+  basePath: '/Liturgia', // Replace with your GitHub repository name
+  assetPrefix: '/Liturgia/', // Ensures assets are loaded correctly
+};
+
+module.exports = nextConfig;
